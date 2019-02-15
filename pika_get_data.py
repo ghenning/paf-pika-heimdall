b0c930c9e891c96474ab236ca9bca5ea43cab2dd
prajwalvp <prajwal3108@gmail.com>
2019-02-15 12:35:16 +0100

    added node affinity in paf_hd_proc.yaml

--- pika_get_data.py
+++ pika_get_data.py
@@ -6,7 +6,9 @@ import pika_process as PP
 def main(producer, path):
     #thepath = os.path.join(path,"*_8bit.fil")
     #thepath = os.path.join(opts.path,"*/","*_8bit.fil") # dir with subdirs
-    thepath = os.path.join(opts.path,"2018-04*/","*_8bit.fil") # April run dirs
+    #thepath = os.path.join(opts.path,"2018-04*/","*_8bit.fil") # April run dirs
+    #thepath = os.path.join(opts.path,"2018-09-01_233441/B0609+37_180902_5_2/","*.fil") # test with concatenated PAF data
+    thepath = os.path.join(opts.path,"*.fil") # test with concatenated PAF data
 
     #do shit with glob results
     producer.publish(glob.glob(thepath))
--- pika_heimdall_wrapper.py
+++ pika_heimdall_wrapper.py
@@ -5,7 +5,7 @@ import sys
 import time
 import signal
 import pika_process as PP
-import thechecker
+#import thechecker
 
 def printit(msg):
     print msg
@@ -18,8 +18,10 @@ def on_message(body, opts):
     #outdir = "/output/{}".format(filename.split("/")[2]) # full path since we're mounting beegfs to beegfs
     outdir = "/beegfs/heimpaf/processed/{}".format(filename.split("/")[-2]) # full path since we're mounting beegfs to beegfs
     # NB: the outdir thing might need some work to make it a bit cleaner/fancier
-    if thechecker.check_between(body,outdir):
-        return
+    # thechecker only works for the original naming convention, it's not the end of the world if we
+    # end up re-running some stuff...
+    #if thechecker.check_between(body,outdir):
+        #return
     try:
         os.mkdir(outdir)
     except OSError as error:
@@ -28,7 +30,20 @@ def on_message(body, opts):
     printit( "Calling Heimdall...")
     subprocess.check_call(["/heimdall/Applications/heimdall", "-f", filename,
                            "-gpu_id", gpu, "-dm", str(opts.lodm), str(opts.hidm),
+                           #"-zap_chans", str(330), str(400),
+                           #"-zap_chans", str(425), str(440),
+                           #"-zap_chans", str(470), str(495),
+                           #"-zap_chans", str(500), str(512),
                            "-detect_thresh", str(opts.thresh), "-output_dir", outdir])
+                           #"-zap_chans", str(330), str(512),
+                           #"-detect_thresh", str(opts.thresh), "-output_dir", outdir,
+                           #"-zap_chans", str(329), str(400), # to deal with the shitty lower half of band
+                           #"-zap_chans", str(424), str(440), 
+                           #"-zap_chans", str(469), str(495), 
+                           #"-zap_chans", str(499), str(511)])
+                           #"-detect_thresh", str(opts.thresh), "-output_dir", outdir])
+                           #"-zap_chans", str(329), str(400), "-zap_chans", str(424), str(440), "-zap_chans", str(469), str(495),
+                           #"-zap_chans", str(499), str(511)])
     subprocess.check_call(['chown','-R','50000:50000',str(outdir)])
 
 if __name__ == "__main__":
